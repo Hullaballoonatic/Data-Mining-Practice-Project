f8ead398c1dcf57a2aa57b965485cd813e222bf3
Casey Stratton <cstratto@email.uark.edu>
2019-02-28 20:49:50 -0600

    got a tree built and the prediction printed out.

--- 1/a.py
+++ 1/a.py
@@ -2,28 +2,33 @@ import graphviz
 import pandas as pd
 from sklearn import tree
 
-# Grab feature names
 with open('1/features.csv') as file:
     features = [feature.strip() for feature in file.readline().split(',')]
 
-# Grab training data
 train_df = pd.read_csv('1/training.csv', dtype='category')
 train_samples = pd.get_dummies(train_df.loc[:, features[:-1]])
 train_labels = pd.get_dummies(train_df.loc[:, features[-1]])
 
-# Create the tree
-clf = tree.DecisionTreeClassifier().fit(train_samples, train_labels)
-
-# View the tree
-graphviz.Source(tree.export_graphviz(clf, out_file=None)).render(filename='treeclassifier', directory='1', view=True)
-
-# Grab testing data
 test_df = pd.read_csv('1/testing.csv', dtype='category')
 test_samples = pd.get_dummies(test_df.loc[:, features[:-1]])
 test_labels = pd.get_dummies(test_df.loc[:, features[-1]])
 
-# Get predictions from the testing samples
+missing = set(train_samples) - set(test_samples)
+for attribute in missing:
+    test_samples[attribute] = pd.Series(0, dtype='uint8', index=test_samples.index)
 
-# Compare predicted labels to test labels
 
-# Compute accuracy
+clf = tree.DecisionTreeClassifier().fit(train_samples, train_labels)
+
+# View the tree
+# graphviz.Source(tree.export_graphviz(clf, out_file=None)).render(filename='treeclassifier', directory='1', view=True)
+
+predictions = clf.predict(test_samples)
+
+comparison = test_labels == predictions
+
+numCorrect = comparison.loc[:, '<=50K'].values.tolist.count(True)
+
+accuracy = float(numCorrect) / float(len(predictions))
+
+print(accuracy)
